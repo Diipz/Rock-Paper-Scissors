05ce01107ebb08a591c50b4380caa6371efd620a
Dipesh <dip_patel100@hotmail.com>
2022-04-14 20:05:28 +0100

    Add playRound function and fix promptPlayerSelection bug

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
 let elements = ["ROCK", "PAPER", "SCISSORS"];
+let playerWinCount = 0;
+let computerWinCount = 0;
 
+playRound();
 
 function computerPlay() {
     let computerSelection = Math.floor(Math.random() * 3);
@@ -11,10 +14,27 @@ function promptPlayerSelection() {
     let initialPlayerSelection = prompt("Select rock, paper or scissors", "");
     let playerSelection = initialPlayerSelection.toUpperCase();
 
-    if (playerSelection != ("ROCK" || "PAPER" || "SCISSORS")) {
-    alert("select a valid input");
-    promptPlayerSelection();
+    if (!(playerSelection == "ROCK" || playerSelection == "PAPER" || playerSelection == "SCISSORS")) {
+        alert("Select a valid response");
+        promptPlayerSelection();
+    }
     return playerSelection;
-    } 
 }
 
+function playRound() {
+    computerSelection = computerPlay();
+    playerSelection = promptPlayerSelection();
+    
+    if (playerSelection == computerSelection) {
+        alert(`${playerSelection} vs ${computerSelection}, DRAW!`);
+        playRound();
+    } 
+    else if (playerSelection == "ROCK" && computerSelection == "SCISSORS" || playerSelection == "SCISSORS" && computerSelection == "PAPER" || playerSelection == "PAPER" && computerSelection == "ROCK") {
+        alert(`${playerSelection} beats ${computerSelection}, YOU WIN!`);
+        playerWinCount++;
+    }
+    else if (playerSelection == "ROCK" && computerSelection == "PAPER" || playerSelection == "SCISSORS" && computerSelection == "ROCK" || playerSelection == "PAPER" && computerSelection == "SCISSORS") {
+        alert(`${playerSelection} loses to ${computerSelection}, YOU LOSE!`);
+        computerWinCount++;
+    }    
+}
